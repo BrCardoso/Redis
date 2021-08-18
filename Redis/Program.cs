50e08bfec3d012eaa98a8a83fced75bbecc3ec01
Bruna Cardoso <bruna.cardoso@divicom.com.br>
2021-08-17 22:34:37 -0300

    Respostas

--- Redis/Program.cs
+++ Redis/Program.cs
@@ -1,5 +1,7 @@
 ﻿using StackExchange.Redis;
 using System;
+using System.Data;
+using System.Text.RegularExpressions;
 
 namespace Redis
 {
@@ -10,13 +12,18 @@ namespace Redis
         static void Main(string[] args)
         {
             var redis = ConnectionMultiplexer.Connect("40.65.216.220");
+            DataTable dt = new DataTable();
             IDatabase db = redis.GetDatabase();
 
             var sub = redis.GetSubscriber();
             sub.Subscribe("perguntas").OnMessage(
                 m => {
-                    var perguntaId = m.Message.ToString().Split(":")[0];
-                    db.HashSet(perguntaId, new[] { new HashEntry("Grupo 4", "Pesquisa no Google") });
+                    var message = m.Message.ToString().Split(":");
+
+                    var calc = Regex.Replace(message[1], "[A-Z|a-z|é]", "").Trim().Replace("?", "");
+                    var result = dt.Compute(calc, "");
+
+                    db.HashSet(message[0], new[] { new HashEntry("Grupo 4", result.ToString()) });
                 }
                 );
 
