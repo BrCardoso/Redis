b2e978be5d467407ecd96961e152c5fddcae60c0
Bruna Cardoso <bruna.cardoso@divicom.com.br>
2021-08-17 22:00:00 -0300

    Add project files.

---
+++ Redis/Program.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Redis
+{
+    class Program
+    {
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+        private static object result;
+        static void Main(string[] args)
+        {
+            var redis = ConnectionMultiplexer.Connect("40.65.216.220");
+            IDatabase db = redis.GetDatabase();
+
+            var sub = redis.GetSubscriber();
+            sub.Subscribe("perguntas").OnMessage(
+                m => {
+                    var perguntaId = m.Message.ToString().Split(":")[0];
+                    db.HashSet(perguntaId, new[] { new HashEntry("Grupo 4", "Pesquisa no Google") });
+                }
+                );
+
+            Console.ReadLine();
+        }
+    }
+}
